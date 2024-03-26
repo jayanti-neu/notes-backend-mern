391e8b6347b9e748eeb20957dcc6e11a5c32e34a
Jayanti Hari <lnu.jay@northeastern.edu>
2024-03-25 21:40:39 -0400

    adds initial routes to router

---
+++ app/routes/index.js
@@ -0,0 +1,7 @@
+import meetingNoteRouter from './meeting-note-route.js'
+
+const initializeRoutes = (app) => {
+    app.use('/meetingNotes', meetingNoteRouter);
+}
+
+export default initializeRoutes;
---
+++ app/routes/meeting-note-route.js
@@ -0,0 +1,8 @@
+import express from "express"
+import * as meetingNoteController from './../controllers/meeting-note-controller.js';
+
+const router = express.Router();
+
+router.route('/')
+    .get(meetingNoteController.findAll)
+    .post(meetingNoteController.post)
